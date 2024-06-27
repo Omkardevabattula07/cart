8fa301a0cd8a2fb6035affd301956daf165d18ab
omkar <devabattulaosst1909@gmail.com>
2024-06-27 19:34:13 +0530

    added the functions and path in views and urls in app

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+def register_view(request):
+    return render(request,'register.html')
+def login_view(request):
+    return render(request,'login.html')
+def wait(request):
+    return render(request,'wait.html')
+def superuser(request):
+    return render(request,'superuser.html')
+def userprofile(request):
+    return render(request,'userprofile.html')
