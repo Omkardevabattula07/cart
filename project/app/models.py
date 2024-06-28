ce40f4aa0a5ab73d45e0aecabdb3d4ee340933ea
omkar <devabattulaosst1909@gmail.com>
2024-06-28 20:44:44 +0530

    Added a superuser and models and added the modelname in the admin

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Users(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    bio=models.TextField(blank=False)
+    profile_pic=models.ImageField(upload_to='profile_pics/')
+    is_approved=models.BooleanField(default=False)
+    def __str__(self):
+        return self.user.username
